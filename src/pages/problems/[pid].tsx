5daf4c0111ae6b60ff324acdcd6e3dc13b1f04e5
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-11-02 19:48:45 +0530

    hero

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import Topbar from "./src/components/Topbar/Topbar";
+import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
 import useHasMounted from "@/hooks/useHasMounted";
 import { problems } from "@/utils/problems";
