c7dbc588e822a257036bf5069fbc49fa88c5fbe4
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-11-02 19:46:31 +0530

    done

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -96,8 +96,6 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 					<div>
 						<a
 							href='/code-translate'
-							target='_blank'
-							rel='noreferrer'
 							className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
 						>
 							Translate
